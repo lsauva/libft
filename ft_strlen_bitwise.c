019fd80b15e9adbc1bdf2b5e577fc0861dea3b3d
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-02-07 19:29:59 +0100

    bitwise version of ft_strlen

--- ft_strlen_bitwise.c
+++ ft_strlen_bitwise.c
@@ -6,28 +6,29 @@
 /*   By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/06 16:34:07 by lsauvage          #+#    #+#             */
-/*   Updated: 2018/02/06 19:58:05 by lsauvage         ###   ########.fr       */
+/*   Updated: 2018/02/07 19:28:32 by lsauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
 
-size_t ft_strlen_bitwise(const char *str)
+size_t      ft_strlen_bitwise(const char *str)
 {
-    size_t len;
-    
+    size_t  len;
+
     len = 0;
     for (;;)
     {
         unsigned x = *(unsigned *)str;
         if ((x & 0xFF) == 0)
-            return len;
+            return (len);
         if ((x & 0xFF00) == 0)
-            return len + 1;
+            return (len + 1);
         if ((x & 0xFF0000) == 0)
-            return len + 2;
+            return (len + 2);
         if ((x & 0xFF000000) == 0)
-            return len + 3;
-        str += 4, len += 4;
+            return (len + 3);
+        str += 4;
+        len += 4;
     }
 }
