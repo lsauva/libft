eee494caa27f2515a0b54eb3883e0ae735c87727
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-04-27 15:59:18 +0200

    update libft

--- ft_lstadd_end.c
+++ ft_lstadd_end.c
@@ -6,11 +6,20 @@
 /*   By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/10 15:22:31 by lsauvage          #+#    #+#             */
-/*   Updated: 2018/04/20 17:13:53 by lsauvage         ###   ########.fr       */
+/*   Updated: 2018/04/27 15:42:05 by lsauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "./libft.h"
+
+/*
+** DESCRIPTION : Ajoute l’élément new en fin de la liste.
+**
+** PARAM #1 : L’adresse d’un pointeur sur le premier maillon d’une liste.
+** PARAM #2 : Le maillon à ajouter en fin de cette liste.
+**
+** RETOUR : rien.
+*/
 
 void	ft_lstadd_end(t_list **alst, t_list *new)
 {
