a478336c4b764f8c3f900a45646e46cf6090be57
Laurent SAUVAGE <lsauvage@student.42.fr>
2018-05-03 17:27:16 +0200

    update libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lsauvage <lsauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/13 12:27:20 by lsauvage          #+#    #+#             */
-/*   Updated: 2018/04/27 15:42:43 by lsauvage         ###   ########.fr       */
+/*   Updated: 2018/05/02 19:43:14 by lsauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+
 
 typedef struct		s_list
 {
